21f24eedb0b77a1ee041066c2407732065336680
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-07-23 18:44:09 -0400

    updating example page to follow standards.

--- examples/redirection/index.php
+++ examples/redirection/index.php
@@ -1,28 +1,35 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT']."/dist/country-block.inc.php";
-
-// Block Canada, United States, and Congo
-$countries = array("CA","US","CG");
-
-// Api Key
-$api_key = "INSERT-API-KEY-HERE";
-
-// path optional
-$path = $_SERVER['DOCUMENT_ROOT']."/dist/";
-// pass to class as third paramater if required.
-
-// return true if someone has been blocked. false if not
-$countryBlock = new countryBlock($countries, $api_key);
-
-if($countryBlock) { // returned true
-  // Blocked!
-  // Display your access denied content... or redirect...
-  header('Location: /access-denied.php');
-}
-else {
-  // Show them my awesome page!
-  include "./awesome-page.php";
-}
-
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Important Example Page</title>
+  </head>
+  <body>
+    <?php
+    // Require library
+    require_once $_SERVER['DOCUMENT_ROOT']."/dist/country-block.inc.php";
+    
+    // Your Block List. This current list blocks  Canada, United States, and Congo.
+    $countries = array("CA","US","CG");
+    
+    // API Key
+    $api_key = "INSERT-API-KEY-HERE";
+    
+    // path optional... pass to class as third paramater if required.
+    $path = $_SERVER['DOCUMENT_ROOT']."/dist/";
+    
+    // return true if someone has been blocked. false if not
+    $countryBlock = new countryBlock($countries, $api_key);
+    
+    // Check if the user is allowed to be redirected or not.
+    if($countryBlock) {
+      // Blocked!
+      // Display your access denied content...
+      echo "<h2>Access Denied</h2><p>The country you are accessing this website through is not permitted on this website</p>";
+    }
+    else {
+      // Show them my awesome page!
+      include "./awesome-page.php";
+    }
+  ?>
+  </body>
+</html>
