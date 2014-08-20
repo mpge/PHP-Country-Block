2805b05da39177b4e883bdc2ba003e3a2ffd2358
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-08-20 17:03:28 -0400

    A bunch of fixes. See extended description.
    Add regex for new API output, adding Cookie value for further optimizations, and implementing isBlocked to fix construct returning true issue.
    
    For further optimizations, if user is not blocked they too will be given a cookie to remove duplicating ipinfo requests if user reloads page. 
    
    You can fix this by using this script in a sort-of "Gateway mode" whereas the page determines if the user is blocked or not and redirects instead of displaying content on the same page. However, a fix will be implemented soon.

--- dist/country-block.inc.php
+++ dist/country-block.inc.php
@@ -1,138 +1,144 @@
 <?php
 /**
-  *
-  * @author Matt Gross <contact@mattgross.net>
-  * @link https://github.com/MatthewGross/PHP-Country-Block
-  * @license MIT - http://mattgross.mit-license.org/
-  *
-  * A basic PHP IP Address blocker for certain countries provided by the user.
-  * Uses IP Info DB <http://ipinfodb.com/>
-  * Uses IP Info DB PHP Wrapper <http://github.com/beingtomgreen/IP-User-Location>
-  *
-  */
+ *
+ * @author Matt Gross <contact@mattgross.net>
+ * @link https://github.com/MatthewGross/PHP-Country-Block
+ * @license MIT - http://mattgross.mit-license.org/
+ *
+ * A basic PHP IP Address blocker for certain countries provided by the user.
+ * Uses IP Info DB <http://ipinfodb.com/>
+ * Uses IP Info DB PHP Wrapper <http://github.com/beingtomgreen/IP-User-Location>
+ *
+ */
 
-class countryBlock {
-
-  // Store Country
-  private $countries = null;
-  
-  // Store Visitors IP Address
-  private $ip_address = null;
-  
-  // Store ipInfo object
-  private $ipInfo = null;
-  
-  /**
-    * __construct
-    *
-    * @param array $countries - All countries provided in an array
-    * @param string $apiKey - Your API key
-    * @param string $path_to_script - Directory path before the Uses IP Info DB PHP Wrapper
-    *
-    * @return true/false - true if ip has been blocked, false if ip has not been blocked and is permitted.
-    * 
-    */
-  public function __construct($countries, $api_key, $path_to_script = '')
-  {
-    // Include ipInfo.inc.php with or without path
-    include($path_to_script.'ipInfo.inc.php');
+class countryBlock
+{
     
-    // new ipInfo class with api_key from parameters
-    $this->ipInfo = new ipInfo($api_key);
-  
-    // Save Countries to $countries
-    $this->countries = $countries;
+    // Store Country
+    private $countries = null;
     
-    // Save IP Address $ip_address
-    $this->ip_address = $this->ipInfo->getIPAddress();
+    // Store Visitors IP Address
+    private $ip_address = null;
     
-    // Check if cookie exists
-    if($this->cookieCheck())
+    // Store ipInfo object
+    private $ipInfo = null;
+    
+    // Returning line. Sets to true if user is blocked.
+    public $isBlocked = false;
+    
+    /**
+     * __construct
+     *
+     * @param array $countries - All countries provided in an array
+     * @param string $apiKey - Your API key
+     * @param string $path_to_script - Directory path before the Uses IP Info DB PHP Wrapper
+     *
+     * @return true/false - true if ip has been blocked, false if ip has not been blocked and is permitted.
+     * 
+     */
+    public function __construct($countries, $api_key, $path_to_script = '')
     {
-      // returned true... cookie does not exist
-      foreach($this->countries as $country)
-      {
-        // return true or false
-        $blockable = $this->countryCheck($country);
+        // Include ipInfo.inc.php with or without path
+        include($path_to_script . 'ipInfo.inc.php');
         
-        // check
-        if($blockable)
-        {
-          // block
-          $this->setCookie();
-          return true;
+        // new ipInfo class with api_key from parameters
+        $this->ipInfo = new ipInfo($api_key);
+        
+        // Save Countries to $countries
+        $this->countries = $countries;
+        
+        // Save IP Address $ip_address
+        $this->ip_address = $this->ipInfo->getIPAddress();
+        
+        // Check if cookie exists
+        if ($this->cookieCheck()) {
+            // returned true... cookie does not exist
+            foreach ($this->countries as $country) {
+                // return true or false
+                $blockable = $this->countryCheck($country);
+                
+                // check
+                if ($blockable) {
+                    // block
+                    $this->setCookie("true");
+                    // Change the return line.
+                    $this->isBlocked = true;
+                }
+            }
+        } else {
+            // Block again. If the cookie is set, it means the user has been blocked before due to country and should be blocked again.
+            $this->isBlocked = true;
         }
-      }
-      return false;
     }
-    else
-    {
-      // don't block.. Already blocked...
-      return true;
-    }
-  }
-  
-  /**
-    * getCountry()
-    *
-    * Returns true or false based off if the country from the ip is in the $countries array
-    *
-    * @param string $country - country code of one country!
-    *
-    * @return true/false - true if same, false if not
-    *
-    */
-  protected function countryCheck($country)
-  {
-    // Get Country from ipInfo API
-    $userCountry = $this->ipInfo->getCountry($this->ip_address);
     
-    // Compare...
-    if($country == $userCountry['countryCode'])
+    /**
+     * getCountry()
+     *
+     * Returns true or false based off if the country from the ip is in the $countries array
+     *
+     * @param string $country - country code of one country!
+     *
+     * @return true/false - true if same, false if not
+     *
+     */
+    protected function countryCheck($country)
     {
-      // Should block
-      return true;
+        // Get Country from ipInfo API
+        $userCountry = $this->ipInfo->getCountry($this->ip_address);
+        
+        // Using regex, take specific info out of returned string at $userCountry
+        $regex = ".*?((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d]).*?((?:[a-z][a-z]+)).*?((?:[a-z][a-z]+))";
+        
+        if ($c = preg_match_all("/" . $regex . "/is", $userCountry, $matches)) {
+            $ipAddress   = $matches[1][0];
+            $countryCode = $matches[2][0];
+            $countryName = $matches[3][0];
+            
+            // Compare...
+            if ($country == $countryCode) {
+                // Should block
+                return true;
+            } else {
+                // Shouldn't block
+                return false;
+            }
+        }
+        return false;
+        
     }
-    else
+    
+    /**
+     * cookieCheck()
+     *
+     * Returns true or false depending if the cookie ('ip_not_allowed') is set or not
+     *
+     * @return true/false - true if not set, false if is set
+     *
+     */
+    protected function cookieCheck()
     {
-      // Shouldn't block
-      return false;
+        if (!isset($_COOKIE['ip_not_allowed'])) {
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
-  
-  /**
-    * cookieCheck()
-    *
-    * Returns true or false depending if the cookie ('ip_not_allowed') is set or not
-    *
-    * @return true/false - true if not set, false if is set
-    *
-    */
-  protected function cookieCheck()
-  {    
-    if(!isset($_COOKIE['ip_not_allowed']))
+    
+    /**
+     * setCookie()
+     *
+     * Sets cookie and returns true
+     *
+     * @param string $value - Value to set the cookie. "true/false"
+     *
+     * @return true
+     *
+     */
+    protected function setCookie($value)
     {
-      return true;
+        setcookie('ip_not_allowed', $value);
+        $_COOKIE['ip_not_allowed'] = $value;
+        return true;
     }
-    else
-    {
-      return false;
-    }
-  }
-  
-  /**
-    * setCookie()
-    *
-    * Sets cookie and returns true
-    *
-    * @return true
-    *
-    */
-  protected function setCookie()
-  {
-    setcookie('ip_not_allowed', 'true');
-    $_COOKIE['ip_not_allowed'] = 'true';
-    return true;
-  }
-
+    
 }
